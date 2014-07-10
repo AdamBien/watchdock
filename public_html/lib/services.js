492010535ac176d42ce77e91b85467fd97858d7d
Adam Bien <abien@adam-bien.com>
2014-07-10 19:13:12 +0200

    Logs are displayed in a textarea

--- public_html/lib/services.js
+++ public_html/lib/services.js
@@ -2,6 +2,7 @@ docker.factory('Connection', function() {
     return {uri: "http://localhost:5555/"};
 });
 
+
 docker.factory('Rest', function($http) {
     return{
         get: function(resource, callback) {
