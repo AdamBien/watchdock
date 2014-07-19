2bf9f6c94759a02626efb2f7085c3d2b61c2e6ec
Adam Bien <abien@adam-bien.com>
2014-07-19 05:18:07 +0200

    Refactoring + computeUri tested

--- test/js/watchdock.spec.js
+++ test/js/watchdock.spec.js
@@ -1,7 +1,25 @@
-beforeEach(module('docker'));
-
-describe('A suite', function() {
-    it('contains spec with an expectation', function() {
-        expect(true).toBe(true);
+describe("watchdock suite", function() {
+    beforeEach(function() {
+        module('docker');
+        inject(function($injector) {
+        });
     });
+
+    describe('computeUri', function() {
+        it('context and reminder should be concatenated with slash in between', function() {
+            var expected = 'context/reminder';
+            expect(computeUri('context', 'reminder')).toEqual(expected);
+        });
+
+        it('existing slash in context should be ignored', function() {
+            var expected = 'context/reminder';
+            expect(computeUri('context/', 'reminder')).toEqual(expected);
+        });
+
+        it('context shorter than 2 characters is not evaluated', function() {
+            var expected = 'c/reminder';
+            expect(computeUri('c/', 'reminder')).toEqual(expected);
+        });
+    });
+
 });
