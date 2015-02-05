b4f2a1e5ed44e2e96a9a525c772fe074d12bc79d
Adam Bien <abien@adam-bien.com>
2015-02-05 17:56:30 +0100

    Docker info integrated

--- public_html/app/controllers.js
+++ public_html/app/controllers.js
@@ -1,4 +1,4 @@
-var computeUri = function(baseUri, reminder) {
+var computeUri = function (baseUri, reminder) {
     if (baseUri && baseUri !== '' && baseUri.length > 2) {
         if (baseUri.charAt(baseUri.length - 1) !== '/')
             baseUri += '/';
@@ -6,22 +6,22 @@ var computeUri = function(baseUri, reminder) {
     return baseUri + reminder;
 };
 docker.controller("containerSelectionController",
-        function($scope, $location, Docker, Connection) {
+        function ($scope, $location, Docker, Connection) {
             $scope.host = Connection;
             $scope.error = false;
-            var errorHandler = function(data) {
+            var errorHandler = function (data) {
                 $scope.error = true;
             };
-            $scope.fetchContainers = function() {
+            $scope.fetchContainers = function () {
                 Connection.update($scope.host.uri);
                 var promise = Docker.get(computeUri(Connection.uri, "containers/json"));
-                promise.then(function(data) {
+                promise.then(function (data) {
                     $scope.containers = data;
                     $scope.error = false;
                 }, errorHandler);
             }
 
-            $scope.containerSelected = function(containerName, containerId) {
+            $scope.containerSelected = function (containerName, containerId) {
                 var path = "/" + containerId;
                 $location.path(path);
                 console.log("Navigating to container: " + containerName + " with name: " + path);
@@ -29,46 +29,46 @@ docker.controller("containerSelectionController",
         }
 );
 docker.controller("containerController",
-        function($scope, $routeParams, Docker, Connection) {
+        function ($scope, $routeParams, Docker, Connection) {
 
             $scope.host = Connection;
             $scope.error = false;
             var containerId = $routeParams.containerId;
             console.log(containerId, $scope, Connection);
             var path = "containers/" + containerId + "/json";
-            var errorHandler = function(data) {
+            var errorHandler = function (data) {
                 $scope.error = true;
             };
             var detailsPromise = Docker.get(computeUri(Connection.uri, path));
             detailsPromise.then(
-                    function(data) {
+                    function (data) {
                         $scope.containerDetails = data;
                         $scope.error = false;
                     }, errorHandler);
 
             var topPromise = Docker.get(computeUri(Connection.uri, "containers/" + containerId + "/top?ps_args=aux"));
             topPromise.then(
-                    function(data) {
+                    function (data) {
                         $scope.runtimeInfo = data;
                         $scope.error = false;
                     }, errorHandler);
 
             var changesPromise = Docker.get(computeUri(Connection.uri, "containers/" + containerId + "/changes"));
             changesPromise.then(
-                    function(data) {
+                    function (data) {
                         $scope.changes = data;
                         $scope.error = false;
                     }, errorHandler);
 
             var logsPromise = Docker.get(computeUri(Connection.uri, "containers/" + containerId + "/logs?stderr=1&stdout=1&timestamps=1&follow=0&tail=all"));
             logsPromise.then(
-                    function(data) {
+                    function (data) {
                         var splitted = data.split("\n");
                         $scope.logs = splitted;
                         $scope.error = false;
                     }, errorHandler);
 
-            $scope.classForKind = function(kind) {
+            $scope.classForKind = function (kind) {
                 if (kind === 1)
                     return 'created';
                 else
@@ -76,3 +76,11 @@ docker.controller("containerController",
             };
         }
 );
+
+docker.controller("infoController", function ($scope, Info) {
+    var infoPromise = Info.get();
+    infoPromise.then(function (data) {
+        $scope.info = data;
+    });
+
+});
--- public_html/app/services.js
+++ public_html/app/services.js
@@ -1,4 +1,4 @@
-docker.factory('Connection', function($cookieStore) {
+docker.factory('Connection', function ($cookieStore) {
     var key = 'dockerUri';
     var dockerUri = $cookieStore.get(key);
     console.log(dockerUri);
@@ -8,24 +8,24 @@ docker.factory('Connection', function($cookieStore) {
     }
     return {
         uri: dockerUri,
-        update: function(newUri) {
+        update: function (newUri) {
             $cookieStore.put(key, newUri);
         }
     };
 });
 
 
-docker.factory('Docker', function($http, $q) {
+docker.factory('Docker', function ($http, $q) {
     return{
-        get: function(resource) {
+        get: function (resource) {
             var deferred = $q.defer();
             $http.get(resource).
                     success(
-                            function(data) {
+                            function (data) {
                                 deferred.resolve(data);
                             }
                     ).error(
-                    function(data) {
+                    function (data) {
                         deferred.reject(data);
                     }
             );
@@ -33,4 +33,23 @@ docker.factory('Docker', function($http, $q) {
         }
     };
 
+});
+
+docker.factory('Info', function ($http, $q, Connection) {
+    return{
+        get: function () {
+            var deferred = $q.defer();
+            $http.get(Connection.uri + "info").
+                    success(
+                            function (data) {
+                                deferred.resolve(data);
+                            }
+                    ).error(
+                    function (data) {
+                        deferred.reject(data);
+                    }
+            );
+            return deferred.promise;
+        }
+    }
 });
